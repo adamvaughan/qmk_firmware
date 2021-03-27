0fdaee140dc7035406e44379b6dde869f1a80c44
Adam Vaughan <adamjvaughan@gmail.com>
2021-03-27 14:50:39 -0600

    First pass at lily58 layout.

---
+++ keyboards/lily58/keymaps/adamvaughan/keymap.c
@@ -0,0 +1,148 @@
+#include QMK_KEYBOARD_H
+
+enum layer_number {
+  _QWERTY = 0,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* QWERTY
+  * ,-----------------------------------------.                    ,-----------------------------------------.
+  * | ESC/~|   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |BACKSP|
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
+  * |LSHIFT|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RSHIFT|
+  * `-----------------------------------------/       /     \      \-----------------------------------------'
+  *                   | LALT | LGUI |LOWER | /SPACE  /       \ENTER \  |RAISE | RGUI | RALT |
+  *                   |      |      |      |/       /         \      \ |      |      |      |
+  *                   `----------------------------'           '------''--------------------'
+  */
+  [_QWERTY] = LAYOUT( \
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, \
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, \
+    KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LBRC, KC_RBRC, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+    KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_RGUI, KC_RALT \
+  ),
+
+  /* LOWER
+  * ,-----------------------------------------.                    ,-----------------------------------------.
+  * |   `  |      |      |      |      |      |                    |      |      |      |      |   -  |  =   |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |-------.    ,-------|      | LEFT |RIGHT |  UP  | DOWN |      |
+  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+  * `-----------------------------------------/       /     \      \-----------------------------------------'
+  *                   | LALT | LGUI |LOWER | /       /       \      \  |ADJUST| RGUI | RALT |
+  *                   |      |      |      |/       /         \      \ |      |      |      |
+  *                   `----------------------------'           '------''--------------------'
+  */
+  [_LOWER] = LAYOUT( \
+    KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_EQL, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_LALT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT, MO(_ADJUST), KC_RGUI, KC_RALT \
+  ),
+
+  /* RAISE
+  * ,-----------------------------------------.                    ,-----------------------------------------.
+  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      |      |  UP  |      |      |      |                    |      |      |      |      |      |      |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      | LEFT | DOWN |RIGHT |      |      |-------.    ,-------|      | HOME | END  | PGUP |PGDWN |      |
+  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+  * | MUTE | VOL+ | VOL- |      | BR+  | BR-  |-------|    |-------|      |      |      |      |      |      |
+  * `-----------------------------------------/       /     \      \-----------------------------------------'
+  *                   | LALT | LGUI |ADJUST| /       /       \      \  |RAISE | RGUI | RALT |
+  *                   |      |      |      |/       /         \      \ |      |      |      |
+  *                   `----------------------------'           '------''--------------------'
+  */
+  [_RAISE] = LAYOUT( \
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
+    KC_NO, KC_NO, KC_UP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO, KC_NO, KC_HOME, KC_END, KC_PGUP, KC_PGDN, KC_NO, \
+    KC_MUTE, KC_VOLU, KC_VOLD, KC_NO, KC_BRIU, KC_BRID, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_LALT, KC_LGUI, MO(_ADJUST), KC_SPC, KC_ENT, MO(_RAISE), KC_RGUI, KC_RALT \
+  ),
+
+  /* ADJUST
+  * ,-----------------------------------------.                    ,-----------------------------------------.
+  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
+  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |-------.    ,-------|RGB ON|MODE+ | HUE+ | SAT+ | BR+  | EFF+ |
+  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+  * |      |      |      |      |      |      |-------|    |-------|      |MODE- | HUE- | SAT- | BR-  | EFF- |
+  * `-----------------------------------------/       /     \      \-----------------------------------------'
+  *                   | LALT | LGUI |ADJUST| /       /       \      \  |ADJUST| RGUI | RALT |
+  *                   |      |      |      |/       /         \      \ |      |      |      |
+  *                   `----------------------------'           '------''--------------------'
+  */
+  [_ADJUST] = LAYOUT( \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, \
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,\
+    KC_LALT, KC_LGUI, MO(_ADJUST), KC_SPC, KC_ENT, MO(_ADJUST), KC_RGUI, KC_RALT \
+  )
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+//SSD1306 OLED update loop, make sure to enable OLED_DRIVER_ENABLE=yes in rules.mk
+#ifdef OLED_DRIVER_ENABLE
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master())
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  return rotation;
+}
+
+// When you add source files to SRC in rules.mk, you can use functions.
+const char *read_layer_state(void);
+const char *read_logo(void);
+void set_keylog(uint16_t keycode, keyrecord_t *record);
+const char *read_keylog(void);
+const char *read_keylogs(void);
+
+// const char *read_mode_icon(bool swap);
+// const char *read_host_led_state(void);
+// void set_timelog(void);
+// const char *read_timelog(void);
+
+void oled_task_user(void) {
+  if (is_keyboard_master()) {
+    // If you want to change the display of OLED, you need to change here
+    oled_write_ln(read_layer_state(), false);
+    oled_write_ln(read_keylog(), false);
+    oled_write_ln(read_keylogs(), false);
+    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    //oled_write_ln(read_host_led_state(), false);
+    //oled_write_ln(read_timelog(), false);
+  } else {
+    oled_write(read_logo(), false);
+  }
+}
+#endif // OLED_DRIVER_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+#ifdef OLED_DRIVER_ENABLE
+    set_keylog(keycode, record);
+#endif
+    // set_timelog();
+  }
+  return true;
+}
