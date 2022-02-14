57d885c8d0917c18c67ed767c9d5dcd59dfc3435
Adam Vaughan <adamjvaughan@gmail.com>
2022-02-14 16:51:56 -0700

    updates

--- keyboards/misonoworks/chocolatebar/config.h
+++ keyboards/misonoworks/chocolatebar/config.h
@@ -20,19 +20,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x6D77 // "mW" - misonoWorks
-#define PRODUCT_ID      0xC456
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    MisonoWorks
-#define PRODUCT         Chocolate Bar
+#define VENDOR_ID 0x6D77  // "mW" - misonoWorks
+#define PRODUCT_ID 0xC456
+#define DEVICE_VER 0x0001
+#define MANUFACTURER MisonoWorks
+#define PRODUCT Chocolate Bar
 
 /* key matrix size */
 #define MATRIX_ROWS 4
 #define MATRIX_COLS 16
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { B0, B7, D2, D3 }
-#define MATRIX_COL_PINS { F0, F1, F4, F5, F6, F7, C7, C6, B6, B5, B4, D7, D6, D4, B3, B2 }
+#define MATRIX_ROW_PINS \
+    { B0, B7, D2, D3 }
+#define MATRIX_COL_PINS \
+    { F0, F1, F4, F5, F6, F7, C7, C6, B6, B5, B4, D7, D6, D4, B3, B2 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
@@ -40,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* number of backlight levels */
 #ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 3
+#    define BACKLIGHT_LEVELS 3
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -49,15 +51,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define RGB_DI_PIN B1
 #ifdef RGB_DI_PIN
-#define RGBLED_NUM 12
-#define RGBLIGHT_EFFECT_BREATHING
-#define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#define RGBLIGHT_EFFECT_SNAKE
-#define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLED_NUM 12
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
 // #define RGBLIGHT_EFFECT_CHRISTMAS
-#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
 // #define RGBLIGHT_EFFECT_RGB_TEST
 // #define RGBLIGHT_EFFECT_ALTERNATING
-#define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
